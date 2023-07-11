4d619822d67207cb67b8ca6eb8f0497e8d76b23a
Tryndameir <andrewdinho99@gmail.com>
2023-07-10 20:36:59 -0400

    Completed repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+function repeatString(word, amount) {
+    let i = 0;
+    let newWord = "";
 
-};
+    if (amount >= 0)
+    {
+        for (i = 0; i < amount; i++)
+        {
+            newWord = newWord + word;
+        }
+    return newWord;
+    }
+    else return "ERROR";
+    
+}
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
