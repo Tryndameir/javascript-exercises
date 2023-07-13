0c7a5e252d57fa7bdba31e67befc5a9e9b881e5f
Tryndameir <andrewdinho99@gmail.com>
2023-07-13 00:22:28 -0400

    Created a reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let newString = [];
+    let n = words.length;
+    let i = 0;
+    for (i = 0; i < n; i++)
+    {
+        newString[n - (i + 1)] = words[i];
+    }
+    newString = newString.toString();
+    return newString;
+    
 };
+
+reverseString("go");
 
 // Do not edit below this line
 module.exports = reverseString;
