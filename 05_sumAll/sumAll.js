6a26da6f4591bc7060d3af68084defcb4e5a6dec
Tryndameir <andrewdinho99@gmail.com>
2023-07-14 20:18:52 -0400

    Stared forst test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let x = num2;
+    let y = num1;
+    let i = 0;
+    for (i; i < x + 1; i++)
+    {
+        y = y + 1;
+        y++;
+    }
 
+    return y;
 };
+
+sumAll(1, 4);
 
 // Do not edit below this line
 module.exports = sumAll;
