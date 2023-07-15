e4db831639118c29d8e166b636482e01841ddd6f
Tryndameir <andrewdinho99@gmail.com>
2023-07-15 02:50:07 -0400

    Completed leapYears. All tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,40 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (typeof(year) != 'number') //test if year is a number
+    {
+        return "ERROR";
+    }
+
+    if (year < 0) //test if year is a positive number
+    {
+        return "ERROR";
+    }
+
+    if (year % 4 != 0)
+    {
+        return false;
+    }
+
+    if (year % 4 == 0 && year % 100 != 0) //if year is divisible by 4 return and NOT divisible by 100
+    {
+        return true;
+    }
+
+    if (year % 100 == 0) //if year is divisible by 100 then further divide by 400. if true year is leap year
+    {
+        if (year % 400 == 0)
+        {
+            return true;
+        }
+
+        else // if year is not divisible by 400 then i snot leap year
+        {
+            return false;
+        }
+    }
 
 };
+
+leapYears(1900);
 
 // Do not edit below this line
 module.exports = leapYears;
