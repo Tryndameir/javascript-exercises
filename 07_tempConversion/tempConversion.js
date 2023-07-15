e286dfd8a28113e1cb0465fcc83e65842e23001b
Tryndameir <andrewdinho99@gmail.com>
2023-07-15 03:03:09 -0400

    started conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) { // x °F ≘ (x − 32) × 5 / 9
+  let conversion;
+  conversion = (temp - 32) * (5 / 9);
+  Math.round(conversion * 10) / 10;
+  return conversion;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) { // x °C ≘ (x × 9 / 5 + 32)
+  let conversion;
+  conversion = (temp * 9) / (5 + 32);
+  Math.round(conversion * 10) / 10;
+  return conversion;
 };
+convertToCelsius(140);
+convertToFahrenheit(32);
 
 // Do not edit below this line
 module.exports = {
