5ecb1327fdcd3a932d2018e1825e4c5155adb11f
Tryndameir <andrewdinho99@gmail.com>
2023-07-15 01:30:58 -0400

    Completed tes5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,14 +1,33 @@
-const sumAll = function(num1, num2) {
-    let x = num2;
-    let y = num1;
-    let i = 0;
-    for (i; i < x + 1; i++)
+const sumAll = function(num1, num2) { //1 + 2 + 3 + 4 = 10;
+    let x = num1;
+    let y = num2;
+    if (isNaN(x, y))
     {
-        y = y + 1;
-        y++;
+        return 'ERROR';
+    }
+    let a = 0;
+    let b = 0;
+    if (x > y) 
+    {
+        a = y;
+        b = x;
+    }
+    else 
+    {
+        a = x;
+        b = y;
     }
 
-    return y;
+    let n = b - a;
+    let i = 0;
+    let sum = 0;
+    for (i; i < n + 1; i++)
+    {
+        sum = a + sum;
+        a++;
+    }
+
+    return sum;
 };
 
 sumAll(1, 4);
